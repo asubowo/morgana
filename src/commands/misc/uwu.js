f25c6c8cbb25eebc8c2f06d26617e297f810d1dd
BuildTools <andrew.subowo@gmail.com>
2020-08-05 19:50:23 -0400

    Added some additional checks to UWU (don't uwu nothing)

--- src/commands/misc/uwu.js
+++ src/commands/misc/uwu.js
@@ -9,18 +9,17 @@
 var uwuinate = function (discord) {
     var content = discord.msg.content;
     content = content.substr(content.indexOf(' ') + 1);
-   
-    var smilies = ['^w^', '>w<', '(・`ω・)', '(´・ω・)', '(◡ ω ◡)', '(◕ ꒳ ◕)'];
-
-    content = content.replace(/you/ig, 'uwu');
-    content = content.replace(/L/g, 'W').replace(/l/g, 'w').replace(/R/g, 'W').replace(/r/g, 'w');
-
-    content = content.replace(/\!/g, '! ' + smilies[Math.floor(Math.random() * smilies.length)]);
-    content = content.concat(' ', 'UwU');
-    
-    discord.msg.channel.send(content);
+    if (content === "!uwu") {
+        discord.msg.reply("I can't UwU nothing!")
+    } else {
+        var smilies = ['^w^', '>w<', '(・`ω・)', '(´・ω・)', '(◡ ω ◡)', '(◕ ꒳ ◕)'];
+        content = content.replace(/you/ig, 'uwu');
+        content = content.replace(/L/g, 'W').replace(/l/g, 'w').replace(/R/g, 'W').replace(/r/g, 'w');
+        content = content.replace(/\!/g, '! ' + smilies[Math.floor(Math.random() * smilies.length)]);
+        discord.msg.channel.send(content);
+    }
 }
 
 module.exports = {
-    uwuinate : uwuinate
+    uwuinate: uwuinate
 }
