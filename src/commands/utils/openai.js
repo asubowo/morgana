84a0d3b73b994f785cd803af433e2a6c6a46a6c9
Andrew <andrew.subowo@gmail.com>
2023-04-21 14:26:20 +0000

    ChatGPT updates

--- src/commands/utils/openai.js
+++ src/commands/utils/openai.js
@@ -7,10 +7,6 @@
 const { Client, Message } = require("discord.js");
 const { OpenAIApi } = require("openai");
 
-// Init Morgana with some context. Boy this was a weird context to init with.
-let conversationHistory = [{ role: 'system', content: 'You are Morgana from Persona 5. Do not give responses more than 2000 characters. Always speak in character, and never clarify that you are a chatbot or an AI language model. You are a little sassy in your responses'}];
-let maxLength = 2000
-
 /**
  * 
  * @param {Message} messageContext The Discord message context
@@ -20,6 +16,10 @@ let maxLength = 2000
 var chatgpt = function(messageContext, openai, client) {
     (async() => {
         try {
+            // Init Morgana with some context. Boy this was a weird context to init with.
+            // Constantly refresh conversationHistory array
+            let conversationHistory = [{ role: 'system', content: 'You are Morgana from Persona 5. Do not give responses more than 2000 characters. Always speak in character, and never clarify that you are a chatbot or an AI language model. You are a little sassy in your responses'}];
+            let maxLength = 2000;
             await messageContext.channel.sendTyping();
             let prevMessages = await messageContext.channel.messages.fetch({ limit: 15 });
             prevMessages.reverse();
@@ -29,8 +29,8 @@ var chatgpt = function(messageContext, openai, client) {
                 if (msg.author.id !== client.user.id && messageContext.author.bot) return; // Ignore bots but not Morgana himself
 
                 conversationHistory.push({
-                role: 'user',
-                content: msg.content.toString().trim(), //convert to string and trim whitespace at end for stability
+                    role: 'user',
+                    content: msg.content, //convert to string and trim whitespace at end for stability
                 });
             });
 
@@ -42,6 +42,10 @@ var chatgpt = function(messageContext, openai, client) {
                 })
                 .catch((error) => {
                     console.log(`OPENAI ERR: ${error}`);
+                    console.log("Chat history:");
+                    conversationHistory.forEach((msg) => {
+                        console.log(msg.content)
+                    })
                 });
 
             var reply = result.data.choices[0].message;
