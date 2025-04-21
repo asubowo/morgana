e5fb38416c3867d8235fcbff3f751e1d293a7742
asubowo <andrew.subowo@gmail.com>
2025-04-21 13:12:20 -0400

    fix stocks

--- src/commands/utils/stocks.js
+++ src/commands/utils/stocks.js
@@ -11,60 +11,57 @@ import { logger } from '../../utils/logger.js'
  * 
  * @param {Message} messageContext 
  */
-export function getStonks(messageContext) {
-    var regex = /\$([A-Z])\w{0,4}\b/gim;
-    var stock = messageContext.content.match(regex);
-    console.debug("Detected stocks in message:", stock)
+export async function getStonks(messageContext) {
+  var regex = /\$([A-Z])\w{0,4}\b/gim;
+  var stock = messageContext.content.match(regex);
+  console.debug("Detected stocks in message:", stock)
 
-    for (var index = 0; index < stock.length; index++) {
-        let element = stock[index] + "";
-        element = element.replace('$','');
-        stock[index] = element.toUpperCase();
+  for (var index = 0; index < stock.length; index++) {
+    let element = stock[index] + "";
+    element = element.replace('$', '');
+    stock[index] = element.toUpperCase();
+  }
+  try {
+    const embed = new EmbedBuilder()
+      .setColor('Random')
+      .setTitle('TO THE MOON! 🚀🚀🚀')
+      .setTimestamp()
+      .addFields({ name: 'Stock', value: '\u200b', inline: true }, { name: 'Market Prices', value: '\u200b', inline: true }, { name: '\u200b', value: '\u200b', inline: true });
+
+    let shortNames = [];
+    let marketPrices = [];
+
+    for (var i = 0; i < stock.length; i++) {
+      console.debug("Attempting to retrieve quote for", stock[i])
+      const quote = await yahooFinance.quote(stock[i]);
+      const { regularMarketPrice, shortName, symbol, regularMarketChangePercent } = quote;
+
+      if (shortName == undefined) {
+        shortNames = [...shortNames, symbol];
+      } else {
+        shortNames = [...shortNames, shortName];
+      }
+
+      if (regularMarketPrice == undefined) {
+        marketPrices = [...marketPrices, 'Delayed quote or unknown stock'];
+      } else {
+        marketPrices = [...marketPrices, regularMarketPrice];
+      }
+
+      embed.addFields(
+        { name: shortNames[i].toString(), value: symbol, inline: true },
+        { name: "$" + marketPrices[i].toString(), value: percentRound(regularMarketChangePercent).toString() + '%', inline: true },
+        { name: '\u200b', value: '\u200b', inline: true }
+        // Forced empty entry to make formatting the embed a bit better. Not the best, but since they're forcing string values, it is what it is
+      );
     }
+    return await messageContext.channel.send({ embeds: [embed] });
 
-    (async() => {
-        try {
-            const embed = new EmbedBuilder()
-            .setColor('Random')
-            .setTitle('TO THE MOON! 🚀🚀🚀')
-            .setTimestamp()
-            .addFields( { name: 'Stock', value:'\u200b', inline: true }, { name: 'Market Prices', value:'\u200b', inline: true }, { name: '\u200b', value: '\u200b', inline: true } );
-
-            let shortNames = [];
-            let marketPrices = [];
-
-            for (var i = 0; i < stock.length; i++) {
-                console.debug("Attempting to retrieve quote for", stock[i])
-                const quote = await yahooFinance.quote(stock[i]);
-                const { regularMarketPrice, shortName, symbol, regularMarketChangePercent } = quote;
-                
-                if ( shortName == undefined ) {
-                    shortNames = [...shortNames, symbol];
-                } else {
-                    shortNames = [...shortNames, shortName];
-                }
-
-                if ( regularMarketPrice == undefined) {
-                    marketPrices = [...marketPrices, 'Delayed quote or unknown stock'];
-                } else {
-                    marketPrices = [...marketPrices, regularMarketPrice];
-                }  
-
-                embed.addFields(
-                    { name: shortNames[i].toString(), value: symbol, inline: true},
-                    { name: "$" + marketPrices[i].toString(), value: percentRound(regularMarketChangePercent).toString() + '%', inline: true},  
-                    { name: '\u200b', value: '\u200b', inline: true } 
-                    // Forced empty entry to make formatting the embed a bit better. Not the best, but since they're forcing string values, it is what it is
-                );
-            }
-            return await messageContext.channel.send( { embeds: [ embed ]});
-
-        } catch (err) {
-            logger.info(err);
-            logger.info(err.message);
-            return
-        }
-    })();
+  } catch (err) {
+    logger.info(err);
+    logger.info(err.message);
+    return
+  }
 }
 
 /**
@@ -73,5 +70,5 @@ export function getStonks(messageContext) {
  * @returns A float fixed to 2 decimal places
  */
 function percentRound(num) {
-    return Number.parseFloat(num).toFixed(2);
+  return Number.parseFloat(num).toFixed(2);
 }
